b973878aad3034f389dc4a279702816a00345875
Jun Kimura <jun.kimura@datachain.jp>
2025-03-02 19:12:07 +0900

    fix contract upgrade test
    Signed-off-by: Jun Kimura <jun.kimura@datachain.jp>

--- test/ContractUpgrade.t.sol
+++ test/ContractUpgrade.t.sol
@@ -4,23 +4,23 @@ pragma solidity ^0.8.20;
 import {Test} from "forge-std/Test.sol";
 import {Upgrades} from "openzeppelin-foundry-upgrades/Upgrades.sol";
 import {Options} from "openzeppelin-foundry-upgrades/Options.sol";
-import {LCPClientIASOwnableUpgradeable} from "../contracts/LCPClientIASOwnableUpgradeable.sol";
 
 contract ContractUpgrade is Test {
-    string internal constant rootCAFile = "test/data/certs/Intel_SGX_Attestation_RootCA.der";
-
-    function testUpgrade() public {
+    function testUpgradeIAS() public {
         if (!vm.envOr("TEST_UPGRADEABLE", false)) {
             return;
         }
         Options memory opts;
         opts.constructorData = abi.encode(address(0x01), true);
-        bytes memory rootCACert = vm.readFileBinary(rootCAFile);
-        vm.warp(2524607999);
-        Upgrades.deployUUPSProxy(
-            "LCPClientIASOwnableUpgradeable.sol",
-            abi.encodeCall(LCPClientIASOwnableUpgradeable.initialize, rootCACert),
-            opts
-        );
+        Upgrades.validateImplementation("LCPClientIASOwnableUpgradeable.sol", opts);
+    }
+
+    function testUpgradeZKDCAP() public {
+        if (!vm.envOr("TEST_UPGRADEABLE", false)) {
+            return;
+        }
+        Options memory opts;
+        opts.constructorData = abi.encode(address(0x01), true, 0x01, address(0x01));
+        Upgrades.validateImplementation("LCPClientZKDCAPOwnableUpgradeable.sol", opts);
     }
 }
